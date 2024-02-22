453a2fa3d18e2d3935b9510182b537a8e766c6fa
Roshan54321 <roshanneupane54321@gmail.com>
2024-02-22 16:09:25 +0545

    fixed hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -38,30 +38,6 @@ export default function Hero() {
                                         </span>
                                     </div>
                                 </li>
-                                <li className="mt-6 lg:mt-0">
-                                    <div className="flex">
-                                        <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full">
-                                            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                                            </svg>
-                                        </span>
-                                        <span className="ml-4 text-base font-medium leading-6 text-gray-500">
-                                            Comprehensive Support Documentation
-                                        </span>
-                                    </div>
-                                </li>
-                                <li className="mt-6 lg:mt-0">
-                                    <div className="flex">
-                                        <span className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-green-800 bg-green-100 rounded-full">
-                                            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                                            </svg>
-                                        </span>
-                                        <span className="ml-4 text-base font-medium leading-6 text-gray-500">
-                                            Tools for Enhanced Collaboration
-                                        </span>
-                                    </div>
-                                </li>
                             </ul>
                         </div>
                         <div className="relative mt-16 lg:-mx-4 relative-20 lg:mt-0 flex justify-center">
